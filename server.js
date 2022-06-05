f407905c81dbe67541acb572f1152e10a87e6f20
HauNguyen1301 <106897739+HauNguyen1301@users.noreply.github.com>
2022-06-05 11:29:13 +0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = required("./app");
-const config = required("./app/config");
-
-//start server
-const PORT = config.app.port'
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
